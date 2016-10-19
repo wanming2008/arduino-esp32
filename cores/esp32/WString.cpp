fb0162519f3393c231430748d5fe9f36a05c9da1
Todd Treece <toddtreece@gmail.com>
2016-10-19 16:44:53 -0400

    add forward def for flash string helper in wstring

--- cores/esp32/WString.cpp
+++ cores/esp32/WString.cpp
@@ -44,12 +44,10 @@ String::String(const String &value)
     init();
     *this = value;
 }
-/*
 String::String(const __FlashStringHelper *pstr) {
     init();
     *this = pstr; // see operator =
 }
-*/
 #ifdef __GXX_EXPERIMENTAL_CXX0X__
 String::String(String &&rval)
 {
@@ -200,7 +198,6 @@ String & String::copy(const char *cstr, unsigned int length)
     strcpy(buffer, cstr);
     return *this;
 }
-/*
 String & String::copy(const __FlashStringHelper *pstr, unsigned int length) {
     if (!reserve(length)) {
         invalidate();
@@ -210,7 +207,6 @@ String & String::copy(const __FlashStringHelper *pstr, unsigned int length) {
     strcpy_P(buffer, (PGM_P)pstr);
     return *this;
 }
-*/
 #ifdef __GXX_EXPERIMENTAL_CXX0X__
 void String::move(String &rhs)
 {
@@ -276,7 +272,6 @@ String & String::operator =(const char *cstr)
 
     return *this;
 }
-/*
 String & String::operator = (const __FlashStringHelper *pstr)
 {
     if (pstr) copy(pstr, strlen_P((PGM_P)pstr));
@@ -284,7 +279,6 @@ String & String::operator = (const __FlashStringHelper *pstr)
 
     return *this;
 }
-*/
 // /*********************************************/
 // /*  concat                                   */
 // /*********************************************/
@@ -375,7 +369,6 @@ unsigned char String::concat(double num)
     char* string = dtostrf(num, 4, 2, buf);
     return concat(string, strlen(string));
 }
-/*
 unsigned char String::concat(const __FlashStringHelper * str) {
     if (!str) return 0;
     int length = strlen_P((PGM_P)str);
@@ -386,7 +379,6 @@ unsigned char String::concat(const __FlashStringHelper * str) {
     len = newlen;
     return 1;
 }
-*/
 /*********************************************/
 /*  Concatenate                              */
 /*********************************************/
@@ -480,14 +472,12 @@ StringSumHelper & operator +(const StringSumHelper &lhs, double num)
     }
     return a;
 }
-/*
 StringSumHelper & operator + (const StringSumHelper &lhs, const __FlashStringHelper *rhs)
 {
     StringSumHelper &a = const_cast<StringSumHelper&>(lhs);
     if (!a.concat(rhs))	a.invalidate();
     return a;
 }
-*/
 // /*********************************************/
 // /*  Comparison                               */
 // /*********************************************/
